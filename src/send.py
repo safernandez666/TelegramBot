f5f75acf9d09fc7c65fcf977007751d39f1dd020
Santiago Fernandez <santiagoagustinfernandez@gmail.com>
2020-12-31 16:22:45 -0300

    Logging Schedule

--- src/send.py
+++ src/send.py
@@ -6,6 +6,7 @@ import json, logging, time, telegram, os, schedule
 """ Variables """
 today = datetime.today().strftime('%Y%m%d')
 token = os.getenv("TOKEN_TELEGRAM")
+token_vuldb = os.getenv("TOKEN_VULDB")
 chat_id = 390771484
 
 """ Funciones """
@@ -16,7 +17,7 @@ def get_vulnerabilities(json, today):
             str_titulo = ("TITULO: %s" % json['result'][i]['entry']['title'])
             str_id = ("ID: %s" % json['result'][i]['source']['cve']['id'])
             str_riesgo = ("RIESGO: %s" % json['result'][i]['vulnerability']['risk']['name'])
-            texto_string = '\n' + str_titulo + '\n' + str_id + '\n' + str_riesgo + '\n'
+            texto_string = '\n' + str_titulo + '\n' + str_id + '\n' + str_riesgo 
             texto = texto + texto_string
     return texto
 
@@ -25,18 +26,22 @@ def get_notification():
     if (json['response']['status'] == '200'):
         logger.info('API VulDB Status: %s' % json['response']['status'])
         texto = get_vulnerabilities(json, today)
-        return texto
     else:
         logger.warning('API VulDB Status: %s' % json['response']['status'])
         texto = 'Imposible comunicarme con la API'
-        return texto
+    return texto
 
 def send_message(token, chat_id, msg):
-	bot = telegram.Bot(token=token)
-	bot.sendMessage(chat_id=chat_id, text=msg)
+    bot = telegram.Bot(token=token)
+    if len(msg) > 4096: 
+        for x in range(0, len(msg), 4096): 
+            bot.send_message(chat_id=chat_id, text=msg[x:x+4096]) 
+    else: 
+        bot.send_message(chat_id=chat_id, text=msg)
+            
 
 """ Llave API para conectarse a VulnDB & Filtro """
-post_fields	= { 'apikey': os.getenv("TOKEN_VULDB"), 'advisory_date': today }
+post_fields	= { 'apikey': token_vuldb, 'advisory_date': today }
 
 """ Request & Conversion del JSON """
 request = Request('https://vuldb.com/?api', urlencode(post_fields).encode())
@@ -47,8 +52,9 @@ json = json.loads(json_data)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('nAutomaticBot')
 logging.getLogger('schedule').setLevel(logging.CRITICAL + 10)
+logger.info('······ WELCOME TO TELEGRAM BOT ······')
 
-schedule.every().day.at("09:00").do(send_message, token, chat_id, get_notification())
+schedule.every().day.at("16:30").do(send_message, token, chat_id, get_notification())
 
 
 while True:
